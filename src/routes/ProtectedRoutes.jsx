8726eeb7e14d8713531321df724b69ae806fb743
V-U-Simon <V-U-Simon@yandex.ru>
2023-10-30 21:05:28 +0300

    ProtectedRoutes

---
+++ src/routes/ProtectedRoutes.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Outlet } from "react-router";
+import useAuth from "src/hooks/useAuth";
+
+export function ProtectedComponents({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) return <Navigate to="/public" />;
+  return children;
+}
+
+export function ProtectedRoutes() {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) return <Navigate to="/public" />;
+  return <Outlet />;
+}
+
+export function ProtectedUniversal({ children, redirectPath = "/public" }) {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) return <Navigate to={redirectPath} />;
+  return children ? children : <Outlet />;
+}
