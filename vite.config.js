dcb54ec6a949fa5647ed5f849100f4c30f732a31
V-U-Simon <V-U-Simon@yandex.ru>
2023-10-30 17:06:31 +0300

    config: absolute import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+
+// указывает, что импорты относительно этого пути должны рассматриваться как абсолютные
+const aliases = {
+  src: path.resolve(__dirname, "src/"),
+  // src: "/src",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: aliases,
+  },
 });
